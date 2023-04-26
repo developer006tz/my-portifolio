1e9608d0b140aaaa0f3862eef5fac0e78374b5d2
developer006 <developer.ludovic@gmail.com>
2023-04-26 23:50:03 +0300

    add my contact

--- resources/views/components/home/learn-php.blade.php
+++ resources/views/components/home/learn-php.blade.php
@@ -1,17 +1,17 @@
 <x-call-to-action>
   <x-slot:title>
     <h4 class="text-white text-base font-semibold mb-2">
-      Check my PHP Developer Roadmap
+      Want a beautiful Site/App for Your Business !
     </h4>
     <h2 class=" text-white font-bold text-3xl sm:text-[38px] leading-tight mb-6 sm:mb-8 lg:mb-0 ">
-      Start Learning <br class="hidden xs:block"/>
-      PHP For FREE
+      Contact me  <br class="hidden xs:block"/>
+      For Fast and effective work.
     </h2>
   </x-slot>
-    <x-button-link href="https://github.com/developer006tz" variant="dark" class="mr-3" target="_blank">
-      View my channel
+    <x-button-link href="#contact" variant="dark" class="mr-3">
+      Contact me
     </x-button-link>
-    <x-button-link href="https://youtu.be/r9ndOH0tyfA" variant="red" target="_blank">
-      Watch on YouTube
+    <x-button-link href="#portfolio" variant="red">
+      Check my work
     </x-button-link>
 </x-call-to-action>
