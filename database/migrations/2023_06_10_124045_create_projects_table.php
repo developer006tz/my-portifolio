4b06d076e13ef5d08bd683fa347ac0bac177398b
developer006 <developer.ludovic@gmail.com>
2023-06-10 15:55:28 +0300

    add migration for my admin pannel

---
+++ database/migrations/2023_06_10_124045_create_projects_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\ProjectTypes::class)->constrained();
+            $table->string('title');
+            $table->string('image');
+            $table->string('github')->nullable();
+            $table->string('url')->nullable();
+            $table->text('description')->nullable();
+            $table->text('technologies')->nullable();
+            $table->text('features')->nullable();
+            $table->text('challenges')->nullable();
+            $table->text('lessons')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
