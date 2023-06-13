8d84a8311c6fc976a1373eb4cbbb1ba8496908ad
Ludovick Konyo <developer.ludovic@gmail.com>
2023-06-13 13:17:20 +0000

    add new configuration

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -3,27 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Mail\MessageReceived;
-use Illuminate\Http\Request;
 use App\Models\Messages;
+use Illuminate\Http\Request; 
 use Illuminate\Support\Facades\Mail;
 
 class MessageController extends Controller
 {
     public function submit(Request $request)
     {
-        // $validatedData = $request->validate([
-        //     'name' => 'required',
-        //     'email' => 'required|email',
-        //     'message' => 'required',
-        // ]);
-
-        // $message = Messages::create($validatedData);
-
-        // Mail::raw($request->input('message'), function ($mail) use ($request) {
-        //     $mail->from($request->input('email'), $request->input('name'));
-        //     $mail->to('developer@ludovickonyo.com')->subject('New Message');
-        // });
-
         $request->validate([
             'name' => 'required',
             'email' => 'required|email',
@@ -39,7 +26,6 @@ class MessageController extends Controller
         Mail::to('developer@ludovickonyo.com')->send(new MessageReceived($message));
 
 
-
-        return redirect()->route('website.index')->with('successMessage', 'Your message has been sent successfully!');
+        return redirect()->route('website.index')->with('success', 'Your message has been sent successfully!');
     }
 }
