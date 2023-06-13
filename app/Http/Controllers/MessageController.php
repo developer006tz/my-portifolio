2a41b04edf6adb4f0a99ca440a2463dc67f51c77
Ludovick Konyo <developer.ludovic@gmail.com>
2023-06-13 06:56:20 +0000

    add notifcation

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\MessageReceived;
 use Illuminate\Http\Request;
 use App\Models\Messages;
 use Illuminate\Support\Facades\Mail;
@@ -10,18 +11,34 @@ class MessageController extends Controller
 {
     public function submit(Request $request)
     {
-        $validatedData = $request->validate([
+        // $validatedData = $request->validate([
+        //     'name' => 'required',
+        //     'email' => 'required|email',
+        //     'message' => 'required',
+        // ]);
+
+        // $message = Messages::create($validatedData);
+
+        // Mail::raw($request->input('message'), function ($mail) use ($request) {
+        //     $mail->from($request->input('email'), $request->input('name'));
+        //     $mail->to('developer@ludovickonyo.com')->subject('New Message');
+        // });
+
+        $request->validate([
             'name' => 'required',
             'email' => 'required|email',
-            'message' => 'required',
+            'message' => 'required'
         ]);
 
-        $message = Messages::create($validatedData);
+        $message = new Messages;
+        $message->name = $request->name;
+        $message->email = $request->email;
+        $message->message = $request->message;
+        $message->save();
 
-        Mail::raw($request->input('message'), function ($mail) use ($request) {
-            $mail->from($request->input('email'), $request->input('name'));
-            $mail->to('developer@ludovickonyo.com')->subject('New Message');
-        });
+        Mail::to('developer@ludovickonyo.com')->send(new MessageReceived($message));
+
+
 
         return back()->with('successMessage', 'Your message has been sent successfully!');
     }
