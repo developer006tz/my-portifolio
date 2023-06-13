5ad03dea10eea1dc17bdbfc7217f34cf95c59e07
Ludovick Konyo <developer.ludovic@gmail.com>
2023-06-13 13:15:55 +0000

    add new configuration

--- app/Mail/MessageReceived.php
+++ app/Mail/MessageReceived.php
@@ -14,18 +14,27 @@ class MessageReceived extends Mailable
 
     public $message;
 
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
     public function __construct(Messages $message)
     {
         $this->message = $message;
     }
 
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
     public function build()
     {
-        return $this->from($this->message->email, $this->message->name)
-            ->view('admin.components.email')
-            ->subject('New message received!')
-            ->with([
-                'message' => $this->message,
-            ]);
+        return $this->view('admin.components.email')
+                    ->subject('New message received!')
+                    ->with([
+                        'message' => $this->message,
+                    ]);
     }
 }
